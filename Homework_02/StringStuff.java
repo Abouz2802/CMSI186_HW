cd6a55f4ce0e4a5cecefd0fb867a258b46b97aaf
Abouz2802 <31747396+Abouz2802@users.noreply.github.com>
2018-02-26 16:10:09 -0800

    Update StringStuff.java

--- Homework_02/StringStuff.java
+++ Homework_02/StringStuff.java
@@ -1,108 +1,166 @@
-public class Stringstuff{
 
-public static void main(Stings[] args) {}
+import java.util.Set;
+import java.util.LinkedHashSet;
 
+public class StringStuff {
 
-public static boolean containsVowel(String s) {
-
-  Strings[] vowels = {"A", "E", "I", "O", "U", "Y", "a", "e", "i", "o", "u", "y"};
-
-  for (int = 0; i < vowels.length; i++)
-    if (s.containts(vowels[i])) {
-      return true
-    }
+  /**
+   * Method to determine if a string contains one of the vowels: A, E, I, O, U, and sometimes Y.
+   * Both lower and upper case letters are handled.  In this case, the normal English rule for Y means
+   * it gets included.
+   *
+   * @param s String containing the data to be checked for &quot;vowel-ness&quot;
+   * @return  boolean which is true if there is a vowel, or false otherwise
+   */
+   public static boolean containsVowel( String s ) {
+     String[] vowels = {"A", "E", "I", "O", "U", "Y", "a", "e", "i", "o", "u", "y"};
+     for (int i = 0; i < vowels.length; i++){
+       if (s.contains(vowels[i])) {
+         return true;
+       }
+     }
       return false;
+   }
+
+  /**
+   * Method to determine if a string is a palindrome.  Does it the brute-force way, checking
+   * the first and last, second and last-but-one, etc. against each other.  If something doesn't
+   * match that way, returns false, otherwise returns true.
+   *
+   * @param s String containing the data to be checked for &quot;palindrome-ness&quot;
+   * @return  boolean which is true if this a palindrome, or false otherwise
+   */
+   public static boolean isPalindrome( String s ) {
+     int i = 0;
+     int j = s.length() - 1;
+     while (i < (s.length() / 2)) {
+       if (s.charAt(i) == s.charAt(j)) {
+         i++;
+         j--;
+       } else {
+           return false;
+     }
+     }
+     return true;
+   }
+
+  /**
+   * Method to return the characters in a string that correspond to the &quot;EVEN&quot; index
+   * numbers of the alphabet.  The letters B, D, F, H, J, L, N, P, R, T, V, X, and Z are even,
+   * corresponding to the numbers 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, and 26.
+   *
+   * @param s String containing the data to be parsed for &quot;even&quot; letters
+   * @return  String containing the &quot;even&quot; letters from the input
+   */
+   public static String evensOnly( String s ) {
+     char[] evens = {'B', 'D', 'F', 'H', 'J', 'L', 'N', 'P', 'R', 'T', 'V', 'X', 'Z',
+                     'b', 'd', 'f', 'h', 'j', 'l', 'n', 'p', 'r', 't', 'v', 'x', 'z'};
+
+     char[] output = new char[s.length()];
+     int k = 0;
+     for(int i = 0; i < s.length(); i++) {
+       for(int j = 0; j < evens.length; j++) {
+         if(s.charAt(i) == evens[j]) {
+           output[k] = s.charAt(i);
+           k++;
+           break;
+         }
+       }
+     }
+     return new String (output).trim();
+   }
+
+  /**
+   * Method to return the characters in a string that correspond to the &quot;ODD&quot; index
+   * numbers of the alphabet.  The letters A, C, E, G, I, K, M, O, Q, S, U, W, and Y are odd,
+   * corresponding to the numbers 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, and 25.
+   *
+   * @param s String containing the data to be parsed for &quot;odd&quot; letters
+   * @return  String containing the &quot;odd&quot; letters from the input
+   */
+   public static String oddsOnly( String s ) {
+     char[] odds = {'A', 'C', 'E', 'G', 'I', 'K', 'M', 'O', 'Q', 'S', 'U', 'W', 'Y',
+                    'a', 'c', 'e', 'g', 'i', 'k', 'm', 'o', 'q', 's', 'u', 'w', 'y'};
+     char[] output = new char[s.length()];
+     int k = 0;
+     for(int i = 0; i < s.length(); i++) {
+       for(int j = 0; j < odds.length; j++) {
+         if(s.charAt(i) == odds[j]) {
+           output[k] = s.charAt(i);
+           k++;
+           break;
+         }
+       }
+     }
+     return new String (output).trim();
+   }
+
+  /**
+   * Method to return the characters in a string that correspond to the &quot;EVEN&quot; index
+   * numbers of the alphabet, but with no duplicate characters in the resulting string.
+   *
+   * @param s String containing the data to be parsed for &quot;even&quot; letters
+   * @return  String containing the &quot;even&quot; letters from the input without duplicates
+   */
+   public static String evensOnlyNoDupes( String s ) {
+      return new String( "HJ" );
+   }
+
+  /**
+   * Method to return the characters in a string that correspond to the &quot;ODD&quot; index
+   * numbers of the alphabet, but with no duplicate characters in the resulting string.
+   *
+   * @param s String containing the data to be parsed for &quot;odd&quot; letters
+   * @return  String containing the &quot;odd&quot; letters from the input without duplicates
+   */
+   public static String oddsOnlyNoDupes( String s ) {
+      return new String( "IK" );
+   }
+
+  /**
+   * Method to return the reverse of a string passed as an argument
+   *
+   * @param s String containing the data to be reversed
+   * @return  String containing the reverse of the input string
+   */
+   public static String reverse( String s ) {
+     int j = s.length() - 1;
+     char[] output = new char[s.length()];
+     for(int i = 0; i < s.length(); i++) {
+       output[i] = s.charAt(j);
+       j--;
+     }
+     return new String(output);
+   }
+ }
+
+  /**
+   * Main method to test the methods in this class
+   *
+   * @param args String array containing command line parameters
+
+   public static void main( String args[] ) {
+      String blah = new String( "Blah blah blah" );
+      String woof = new String( "BCDBCDBCDBCDBCD" );
+      String pal1 = new String( "a" );
+      String pal2 = new String( "ab" );
+      String pal3 = new String( "aba" );
+      String pal4 = new String( "amanaplanacanalpanama" );
+      String pal5 = new String( "abba" );
+      System.out.println( containsVowel( blah ) );
+      System.out.println( containsVowel( woof ) );
+      System.out.println( isPalindrome( pal1 ) );
+      System.out.println( isPalindrome( pal2 ) );
+      System.out.println( isPalindrome( pal3 ) );
+      System.out.println( isPalindrome( pal4 ) );
+      System.out.println( isPalindrome( pal5 ) );
+      System.out.println( "evensOnly()        returns: " + evensOnly( "REHEARSALSZ" ) );
+      System.out.println( "evensOnly()        returns: " + evensOnly( "REhearSALsz" ) );
+      System.out.println( "evensOnlyNoDupes() returns: " + evensOnlyNoDupes( "REhearSALsz" ) );
+      System.out.println( "oddsOnly()         returns: " + oddsOnly( "xylophones" ) );
+      System.out.println( "oddsOnly()         returns: " + oddsOnly( "XYloPHonES" ) );
+      System.out.println( "oddsOnlyNoDupes()  returns: " + oddsOnlyNoDupes( "XYloPHonES" ) );
+      System.out.println( "reverse()          returns: " + reverse( "REHEARSALSZ" ) );
+   }
 }
-
-
-public static boolean isPalindrome( String s) {
-  int i = 0;
-  int j = s.length() - 1;
-  while (i < (s.length() / 2)) {
-    if (s.charAt(i) == s.charAt(j)) {
-      i++;
-      j--;
-    } else {
-        return false;
-  }
-  }
-  return true;
-}
-
-
-public static String evensOnly (String s) {
-  char[] evens = {'B', 'D', 'F,','H', 'J', 'L', 'N', 'P', 'R', 'T', 'V', 'X', 'Z',
-                  'b', 'd', 'f', 'h', 'j', 'l', 'n', 'p', 'r', 't', 'v', 'x', 'z'}
-
-  char[] output = new char[s.length()];
-  int k = 0;
-  for(int i = 0; int < s.length; i++) {
-    for(int j = 0; j < evens.length; j++) {
-      if(s.charAt(i) == evens[j]) {
-        output[k] = s.charAt(i);
-        k++;
-        break;
-      }
-    }
-  }
-  return new String (output).trim();
-}
-
-
-public static String oddsOnly(String s) {
-  char[] output = new char[s.length()];
-  int k = 0;
-  for(int i = 0; int < s.length; i++) {
-    for(int j = 0; j < odds.length; j++) {
-      if(s.charAt(i) == odds[j]) {
-        output[k] = s.charAt(i);
-        k++;
-        break;
-      }
-    }
-  }
-  return new String (output).trim();
-}
-
-public static String evensOnlyNoDupes(String s) {
-  return new string(removeDupes(evensOnly(s)));
-}
-
-public static String oddsOnlyNoDupes(String s) {
-  return new string(removeDupes(oddsOnly(s)));
-}
-
-public static String removeDupes(String s) {
-  StringBuilder sb = new StringBuilder();
-  char[] charRA = s.toCharArray();
-  Set<Character> charSet = new LinkedHashSet<character>();
-  for(char c : charRA) {
-    charSet.add(c);
-  }
-  for(Character myChar : charSet) {
-    sb.append(myChar);
-  }
-  return sb.toString();
-  }
-
-
-  public static long characterCounter(char c, String s) {
-    long count = 0;
-    for(int i = 0; i < s.length(); i++) {
-      if(c == s.charAt(i)) {
-        count++;
-      }
-    }
-    return count;
-  }
-
-  public static String reverse(Strings) {
-    int j = s.length() - 1;
-    char[] output - new char[s.length()];
-    for(int i - 0; 9 < s.length(); i++) {
-      output[i] = s.charAt(j);
-      j--;
-    }
-    return new String(output);
-  }
-}
+*/
