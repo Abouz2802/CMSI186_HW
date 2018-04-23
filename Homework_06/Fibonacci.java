7a66d3473644202b6c9bfed315433bf0ef31e7ae
Abouz2802 <31747396+Abouz2802@users.noreply.github.com>
2018-04-22 21:36:53 -0700

    Update Fibonacci.java

--- Homework_06/Fibonacci.java
+++ Homework_06/Fibonacci.java
@@ -1,1 +1,31 @@
+public class Fibonacci{
+  
+  private static BrobInt answer = new BrobInt("0");
+  private static BrobInt twoBack = BrobInt.ZERO;
+  private static BrobInt oneBack = BrobInt.ONE;
 
+  public static BrobInt getNum(int n)
+  {
+    if(n == 1)
+    {
+      return twoBack;
+    } else if(n == 2)
+      
+    {
+      return oneBack;
+    } else
+    {
+      for(int i=2;i<n;i++)
+      {
+        answer = twoBack.add(oneBack);
+        twoBack = oneBack;
+        oneBack = answer;
+      }
+    }
+    return answer;
+  }
+  public static void main(String args[])
+  {
+    System.out.println(getNum(Integer.parseInt(args[0])));
+  }
+}
